9b2a5a8a47148298e4a9ff8a5f65c0bd0ad1e120
nnarlawar <nnarlawar@dons.usfca.edu>
2021-05-12 17:10:51 -0700

    add files

---
+++ app.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+  ENV = os.getenv('ENV')
+  return f'Hello {ENV}!'
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
